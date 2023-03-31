c7bbbdaa727ecfb92a0568c4c8cb366284d6edb2
Parsa Peikani <104706652+Weekendend@users.noreply.github.com>
2023-03-31 11:48:07 -0700

    Added footer for the register and login modal and created toggle function for the tweet button

--- components/modals/LoginModal.tsx
+++ components/modals/LoginModal.tsx
@@ -1,14 +1,26 @@
 import { useCallback, useState } from "react";
+
 import useLoginModal from "../hooks/useLoginModal";
+import useRegisterModal from "../hooks/useRegisterModal";
+
 import Input from "../Input";
 import Modal from "../Modal";
 
 const LoginModal = () => {
   const loginModal = useLoginModal();
+  const registerModal = useRegisterModal();
 
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [isLoading, setIsLoading] = useState(false);
+
+  const onToggle = useCallback(() => {
+    if (isLoading) {
+      return;
+    }
+    loginModal.onClose();
+    registerModal.onOpen();
+  }, [isLoading, registerModal, loginModal]);
 
   const onSubmit = useCallback(async () => {
     try {
@@ -39,6 +51,21 @@ const LoginModal = () => {
     </div>
   );
 
+  const footerContent = (
+    <div className="text-neutral-400 text-center mt-4">
+      <p>
+        First time using Twitter?
+        <span
+          onClick={onToggle}
+          className="text-white cursor-pointer hover:underline"
+        >
+          {" "}
+          Create an account
+        </span>
+      </p>
+    </div>
+  );
+
   return (
     <Modal
       disabled={isLoading}
@@ -48,6 +75,7 @@ const LoginModal = () => {
       onClose={loginModal.onClose}
       onSubmit={onSubmit}
       body={bodyContent}
+      footer={footerContent}
     />
   );
 };
