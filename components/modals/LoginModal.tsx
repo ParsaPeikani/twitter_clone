b057638faf99cb4f278c5162c8d2bbd5f4e55f1e
Parsa Peikani <104706652+Weekendend@users.noreply.github.com>
2023-04-13 14:55:10 -0700

    Users can login and sign out with the toggle effect on for the log out icon

--- components/modals/LoginModal.tsx
+++ components/modals/LoginModal.tsx
@@ -5,6 +5,7 @@ import useRegisterModal from "../hooks/useRegisterModal";
 
 import Input from "../Input";
 import Modal from "../Modal";
+import { signIn } from "next-auth/react";
 
 const LoginModal = () => {
   const loginModal = useLoginModal();
@@ -25,14 +26,19 @@ const LoginModal = () => {
   const onSubmit = useCallback(async () => {
     try {
       setIsLoading(true);
-      // TODO ADD LOGIN
+
+      await signIn("credentials", {
+        email,
+        password,
+      });
+
       loginModal.onClose();
     } catch (error) {
       console.log(error);
     } finally {
       setIsLoading(false);
     }
-  }, [loginModal]);
+  }, [loginModal, email, password]);
 
   const bodyContent = (
     <div className="flex flex-col gap-4 ">
@@ -44,6 +50,7 @@ const LoginModal = () => {
       />
       <Input
         placeholder="Password"
+        type="password"
         onChange={(e) => setPassword(e.target.value)}
         value={password}
         disabled={isLoading}
